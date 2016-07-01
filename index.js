d8a15df3b11795c1071046a60adfde9fbce4b3e2
ronen-e <alter.ronen@gmail.com>
2016-07-01 22:18:22 +0300

    Add command line arguments

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env node
-console.log('Hello world');
+var program = require('commander');
+program
+    .arguments('<file>')
+    .option('-u, --username <username>', 'The username to authenticate as')
+    .option('-p, --password <password>', 'The user\'s password')
+    .action(function(file) {
+        console.log('user: %s pass: %s file: %s', program.username, program.password, file)
+    })
+    .parse(process.argv);
