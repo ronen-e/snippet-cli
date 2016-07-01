b0fd1e07d10df99296d4617e3230c72650de2e0c
ronen-e <alter.ronen@gmail.com>
2016-07-01 22:30:08 +0300

    Add request to Bitbucket api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node --harmony
+var request = require('superagent');
 var co = require('co');
 var prompt = require('co-prompt');
 var program = require('commander');
@@ -11,7 +12,16 @@ program
         co(function *() {
             var username = yield prompt('username:');
             var password = yield prompt('password:');
-            console.log('user: %s pass: %s file: %s', username, password, file)
+
+            request
+                .post('https://api.bitbucket.org/2.0/snippets')
+                .auth(username, password)
+                .attach('file', file)
+                .set('Accept', 'application/json')
+                .end(function(err, res) {
+                    var link = res.body.links.html.href;
+                    console.log('Snippet created: %s', link);
+                });
         });
     })
     .parse(process.argv);
